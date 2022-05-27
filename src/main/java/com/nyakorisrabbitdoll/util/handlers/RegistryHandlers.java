b027a112cb84bde17d72baf6f5f21b4fcde4cfc8
Administrator <1582017385@qq.com>
2022-05-28 00:31:54 +0800

    1

--- src/main/java/com/nyakorisrabbitdoll/util/handlers/RegistryHandlers.java
+++ src/main/java/com/nyakorisrabbitdoll/util/handlers/RegistryHandlers.java
@@ -12,6 +12,7 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+
 @Mod.EventBusSubscriber
 public class RegistryHandlers {
     @SubscribeEvent
